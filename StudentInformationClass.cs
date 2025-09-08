c44a6f3aed5884cda4cdbe2dacf542b87b05afb8
Rio Unjane Galura <riounjanegalura@gmail.com>
2025-09-08 13:40:56 +0800

    new class

---
+++ StudentInformationClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizationProfile
+{
+    class StudentInformationClass
+    {
+        public static int SetStudentNo = 0;
+        public static int SetContactNo = 0;
+
+        public static String SetProgram = "   ";
+        public static String SetGender = "   ";
+        public static String SetBirthday = "   ";
+        public static String SetFullName = "   ";
+    }
+}
